fb9caeca4c3bbfd39e7091954d9813b5d3946524
Shawn <codingshawn@gmail.com>
2020-11-29 13:44:39 +0800

    Set up basic functionality to switch between home and shop page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { BrowserRouter, Route } from "react-router-dom";
+import Shop from "./components/Shop";
+import Home from "./components/Home";
+import Header from "./components/Header";
+import "./index.css";
+
 function App() {
+  const itemInfo = [{ item: "msw", price: 20 }, {item: "red-prawn", price: 13}];
+
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Route exact path="/" component={Home} />
+        <Route path="/shop">
+          <Shop itemInfo={itemInfo}/>
+        </Route>
+      </div>
+    </BrowserRouter>
   );
 }
 
