ae3692cc581263403203f8a1fd8f1da800cc5a20
Shawn <codingshawn@gmail.com>
2020-11-29 13:51:44 +0800

    Did basic nav bar

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import Navbar from './Navbar'
+
+const Header = () => {
+    return (
+        <header>
+            <h1>
+                Durian Goodness
+            </h1>
+            <Navbar/>
+        </header>
+    )
+}
+
+export default Header
