68fb6848f09f626a2097b57ceca5cf940a497365
Shawn <codingshawn@gmail.com>
2020-11-30 22:39:36 +0800

    Styled home page

---
+++ src/components/Home.js
@@ -0,0 +1,21 @@
+const Home = () => {
+    return(
+        <div className="home-wrapper">
+            <header>
+                <h1>
+                    Durian Goodness
+                </h1>
+                <h2>
+                    Durian so fresh that its finger licking good
+                </h2>
+            </header>
+            <div className="home-img-wrapper">
+                <div className="home-durian home-img">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
